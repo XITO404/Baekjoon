278543b6934c52ac5bad638194e045fa07a933a4
XITO404 <77210435+XITO404@users.noreply.github.com>
2021-07-16 22:27:57 +0900

    7/16

---
+++ 1247.py
@@ -0,0 +1,16 @@
+import sys
+
+for i in range(3):
+    N = int(sys.stdin.readline())
+    arr = []
+    
+    for j in range(N):
+        arr.append(int(sys.stdin.readline()))
+    b = sum(arr)
+    
+    if b > 0:
+        print('+')
+    elif b < 0:
+        print('-')
+    else:
+        print(0)
